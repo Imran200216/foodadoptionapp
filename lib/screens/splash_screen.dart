8bba7b5e6627bb570ef3b72fe2ef1b26271308ab
imran <imranbabuji162002@gmail.com>
2024-10-17 22:56:40 +0530

    Initial commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:foodadoptionapp/constants/colors.dart';
+import 'package:foodadoptionapp/screens/get_started_screen.dart'; // Import your Get Started screen
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Start the timer to navigate to the Get Started screen
+    _startTimer();
+  }
+
+  void _startTimer() {
+    Future.delayed(const Duration(seconds: 4), () {
+      // Navigate to the Get Started screen
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const GetStartedScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    /// media query
+    final sizeHeight = MediaQuery.of(context).size.height;
+    final sizeWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      height: sizeHeight,
+      width: sizeWidth,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.zero,
+        color: AppColors.primaryColor,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/images/svg/splash-logo.svg",
+              height: sizeHeight * 0.18,
+              fit: BoxFit.cover,
+              color: AppColors.secondaryColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

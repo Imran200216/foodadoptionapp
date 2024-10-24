d6e575c654de614e36f66ce7c189ccba3eaaa56e
imran <imranbabuji162002@gmail.com>
2024-10-24 22:30:12 +0530

    toast and debounce helper added successfully.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -6,4 +6,6 @@ class AppColors {
   static Color subtitleColor = const Color(0xFF646A71);
   static Color textFieldBorderColor = const Color(0xFFE8E9E8);
   static Color textFieldHintTextColor = const Color(0xFFABAAAA);
+  static Color successToastColor = const Color(0xFF02A75A);
+  static Color failureToastColor = const Color(0xFFD92F54);
 }

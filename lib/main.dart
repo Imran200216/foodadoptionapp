8df1dfcbf65fe367a5b89f94c24003755fdfcb05
imran <imranbabuji162002@gmail.com>
2024-10-30 21:10:06 +0530

    add the details providers.

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:foodadoptionapp/providers/auth_providers/email_auth_provider.dar
 import 'package:foodadoptionapp/providers/auth_providers/foreget_email_auth_provider.dart';
 import 'package:foodadoptionapp/providers/auth_providers/google_auth_provider.dart';
 import 'package:foodadoptionapp/providers/auth_providers/guest_auth_provider.dart';
+import 'package:foodadoptionapp/providers/details_providers/user_email_details_provider.dart';
+import 'package:foodadoptionapp/providers/details_providers/user_google_details_provider.dart';
+import 'package:foodadoptionapp/providers/details_providers/user_guest_details_provider.dart';
 import 'package:foodadoptionapp/providers/screen_providers/bottom_nav_provider.dart';
 import 'package:foodadoptionapp/providers/screen_providers/get_started_provider.dart';
 import 'package:foodadoptionapp/providers/screen_providers/home_carousel_provider.dart';
 import 'package:foodadoptionapp/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:toastification/toastification.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,17 +59,34 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider(
             create: (context) => HomeCarouselProvider(),
           ),
+
+          /// user email details provider
+          ChangeNotifierProvider(
+            create: (context) => UserEmailDetailsProvider(),
+          ),
+
+          /// user guest details provider
+          ChangeNotifierProvider(
+            create: (context) => UserGuestDetailsProvider(),
+          ),
+
+          /// user google details provider
+          ChangeNotifierProvider(
+            create: (context) => UserGoogleDetailsProvider(),
+          ),
         ],
         builder: (context, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme:
-                  ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
-              useMaterial3: true,
+          return ToastificationWrapper(
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme:
+                    ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+                useMaterial3: true,
+              ),
+              home: const SplashScreen(),
             ),
-            home: const SplashScreen(),
           );
         });
   }
